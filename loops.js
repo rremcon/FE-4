85da1eaaac8a358cbbeb73fa49651100a127688b
rremcon <rremcon@outlook.com>
2022-10-16 00:25:42 +0200

    send

--- loops.js
+++ loops.js
@@ -6,6 +6,9 @@
 // Hoera!
 // ==========================================
 
+for (let i = 0; i < 3; i++) {
+    console.log('Hoera!')
+}
 
 
 // ==========================================
@@ -18,6 +21,13 @@
 // klaar!
  // ==========================================
 
+for (let i = 0; i < 5; i++) {
+    if (i === 4) {
+        console.log('klaar!')
+    } else {
+        console.log('loop...')
+    }
+}
 
 
 // ==========================================
@@ -33,7 +43,14 @@
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
 
-
+for (let i = 0; i < 16; i++) {
+    if (i % 2 === 0) {
+        console.log(i + ' is even');
+    }
+    if (i % 2 !== 0) {
+        console.log(i + ' is oneven');
+    }
+}
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -51,3 +68,12 @@
 // >> 9
 // ==========================================
 
+for (let i = 0; i < 10; i++) {
+    if (i >= 3 && i < 6) {
+        console.log(' > ' + i);
+    } else if(i >= 6) {
+        console.log(' >> ' + i);
+    } else {
+        console.log(i);
+    }
+}
